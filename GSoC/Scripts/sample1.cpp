23e1c63f6e8eca8ca3ec83859851c0c792d7905e
sinatrablue <simonot.robin@gmail.com>
2019-06-10 10:34:51 +0200

    added evaluation function

--- GSoC/Scripts/sample1.cpp
+++ GSoC/Scripts/sample1.cpp
@@ -7,16 +7,78 @@ using namespace std;
 
 ASTNode_t* ast = SBML_parseFormula("k * A");
 
-// Function defining the diff eq
-double df(ASTNode_t *ast){
-    ASTNode *ch1 = ASTNode_getLeftChild(ast);
-    ASTNode *ch2 = ASTNode_getRightChild(ast);
-    double x = ASTNode_getReal(ch1);
-    double y = ASTNode_getReal(ch2);
-    
-    // Because I am not sure if I can get the operand "*" from ast let's start with :
-    double z = y*x;
-    return z;
+double evalAST(ASTNode_t *ast){
+
+    double result;
+    int childnum = ASTNode_getNumChildren(ast);     // gets the number of children (in our case 2, k and A)
+    ASTNode_t  **child = (ASTNode_t **) malloc(childnum * sizeof(ASTNode_t*));     // allocates as much ASTNode_t** as the node has children
+
+    // get every child
+    for(int i=0; i<childnum; i++){
+        child[i] = ASTNode_getChild(ast, i);
+    }
+
+    // switch on the type of ast, the root node (in our case "*")
+    switch(ASTNode_getType(ast)){
+        case AST_INTEGER:
+        int result = ASTNode_getInteger(ast);
+        break;
+
+        case AST_REAL:
+        result = ASTNode_getReal(ast);
+        break;
+
+        case AST_NAME:
+        char *l;
+        double var;
+        printf("Give the value of value : ");
+        printf("%s=", ASTNode_getName(ast));    // prints variable that needs a value
+        cin.getline(l, 20);     // reads value given by user
+        sscanf(l, "%lf", &var);     // stores the float value read in var 
+        delete(l);      // I read it as "free() in C ; delete() in C++" maybe it is not that trivial 
+        printf("%s = %f\n", ASTNode_getName(ast), var);     // confirmation message
+        result = var;
+        break;
+
+        case AST_CONSTANT_FALSE:
+        result = 0.0;
+        break;
+
+        case AST_CONSTANT_TRUE:
+        result = 1.0;
+        break;
+
+        case AST_PLUS:
+        result = evalAST(child[0]) + evalAST(child[1]);
+        break;
+
+        case AST_MINUS:
+        if(childnum==1)
+        result = - (evalAST(child[0]));
+        else
+        result = evalAST(child[0]) - evalAST(child[1]);
+        break;
+
+        case AST_TIMES:     // which is going to be our case
+        result = evalAST(child[0]) * evalAST(child[1]) ;
+        break;
+
+        case AST_DIVIDE:
+        result = evalAST(child[0]) / evalAST(child[1]);
+        break;
+
+        case AST_POWER:
+        result = pow(evalAST(child[0]),evalAST(child[1]));
+        break;
+
+        default :
+        result = 0;
+        break;
+
+    }
+    delete(child);
+
+    return result;
 }
 
 void euler(double t, double y, double dt, double maxt){
@@ -29,7 +91,7 @@ void euler(double t, double y, double dt, double maxt){
 
     while(t<maxt){
         tmp=y;
-        y = y+dt * df(ast);
+        y = y+dt * evalAST(ast);
         t=t+dt;
         outfile<<fixed<<t<<"   "<<fixed<<y<<endl;
 
